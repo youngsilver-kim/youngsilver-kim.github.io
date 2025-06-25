2bc30e63223077bd0800289233c99897820d2e23
youngsilver-kim <tqandyou@gmail.com>
2025-06-25 14:57:46 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,8 @@
 const CACHE_NAME = 'todo-cal-v1';
-const URLS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/manifest.json'
-];
+const URLS = ['/', '/index.html', '/style.css', '/script.js', '/manifest.json'];
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS)));
 });
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(r => r || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
 });
